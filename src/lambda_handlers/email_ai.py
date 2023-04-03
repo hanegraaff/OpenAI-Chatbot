3b9b47c4aa774d409ead5d8b827e1dbb8da039f2
hanegraaff <hanegraaff@gmail.com>
2023-04-02 20:45:35 -0400

    wip

--- src/lambda_handlers/email_ai.py
+++ src/lambda_handlers/email_ai.py
@@ -5,39 +5,30 @@ import os
 import openai
 import datetime
 from parsers.email import EmailMessage
-from connectors import ses
+from email_reply_parser import EmailReplyParser
+
+ses_client = boto3.client('ses')
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-
+    
     try:
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
         log.info(event)
-
-        ses_event = json.loads(event['Records'][0]['Sns']['Message'])
-
+        
         email_msg = None
-        email_msg = EmailMessage(ses_event)
 
-        '''if email_msg.subject.lower().startswith("fw:") or email_msg.subject.lower().startswith("fwd:"):
-            pass
-        else:
-            pass
+        '''ses_event = json.loads(event['Records'][0]['Sns']['Message'])
+        email_msg = EmailMessage(ses_event)
         '''
 
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=email_msg.latest_message,
-            temperature=0.7,
-            max_tokens=250,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            echo=False,
-            best_of=3
+
+        response = openai.ChatCompletion.create(
+            engine="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "write me some comedy in the style of Louie CK"}],
         )
 
         log.info("Choices: [%d]" % len(response.choices))
@@ -51,12 +42,39 @@ def lambda_handler(event, context):
 
         log.info("Email Response:\n%s" % response)
 
-        ses.send_email(email_msg.sent_to, email_msg.sent_from, "RE:%s" % email_msg.subject, response)
+        #send_email(email_msg.sent_from, "RE:%s" % email_msg.subject, response)
+        
+        send_email("hanegraaff@gmail.com", "RE:%s" % email_msg.subject, response)
+        
+       
 
     except Exception as e:
         log.error(e)
         if email_msg is not None:
-            ses.send_email(email_msg.sent_to, email_msg.sent_from, "RE:%s" %
+            send_email(email_msg.sent_from, "RE:%s" %
                        email_msg.subject, "Oh nose! There was an error: %s" % e)
         raise e
 
+
+def send_email(email_to: str, subject: str, body: str):
+
+    response = ses_client.send_email(
+        Source='ask@hal-9001.com',
+        Destination={
+            'ToAddresses': [
+                email_to
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': subject
+            },
+            'Body': {
+                'Text': {
+                    'Data': body,
+                }
+            }
+        }
+    )
+
+    log.info("resonse from ses is: %s" % response)
